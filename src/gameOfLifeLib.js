0a88a6d5888c7a439f9196f3e7f61d6558647e83
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-11-27 13:16:45 +0530

    initial commit

---
+++ src/gameOfLifeLib.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+const handleUserInput = function(){
+  let universeSize = +readline.question("Please enter size of the universe \n");
+  let initialUniverseState = []; 
+}
+
+handleUserInput();
