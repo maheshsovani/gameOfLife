b2e0f6f4f03f66a12e8b568daba7e0c4b6262f19
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-11-27 17:49:52 +0530

    More tests are added to test middle cells.

--- test/utilTest.js
+++ test/utilTest.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const {findOutAllNeighbours} = require('../src/gameOfLifeUtil.js');
 
 describe("findOutAllNeighbours ",function(){
-  it("should return array of three neighbours",function(){
+  it("should return an array of three neighbours",function(){
     let expectedOutput = [[0,1],[1,0],[1,1]];
     assert.deepEqual(findOutAllNeighbours([0,0]),expectedOutput);
     expectedOutput = [[0,1],[1,1],[1,2]];
@@ -12,5 +12,15 @@ describe("findOutAllNeighbours ",function(){
     expectedOutput = [[1,1],[1,2],[2,1]];
     assert.deepEqual(findOutAllNeighbours([2,2]),expectedOutput);
   });
+
+  it("should return an array of eight neighbours",function(){
+    let expectedOutput = [[0, 0], [0,1],[0,2], [1,0], [1,2], [2,0], [2, 1], [2, 2]];
+    assert.deepEqual(findOutAllNeighbours([1,1]),expectedOutput);
+  })
+
+  it("should return an array of five neighbours",function(){
+    let expectedOutput = [[0, 0], [0,1], [1,1], [2,0], [2, 1]];
+    assert.deepEqual(findOutAllNeighbours([1,0]),expectedOutput);
+  })
 });
 
