eec6352ebeca27e36d6042a7e5378b8db3e8cd50
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-11-27 14:36:38 +0530

    implement a function to handle user input.

--- src/gameOfLifeLib.js
+++ src/gameOfLifeLib.js
@@ -2,7 +2,15 @@ const readline = require('readline-sync');
 
 const handleUserInput = function(){
   let universeSize = +readline.question("Please enter size of the universe \n");
-  let initialUniverseState = []; 
+  let initialUniverseState = new Array(universeSize).fill(0).map(x => []); 
+  let requiredGeneration;
+  console.log("Enter the states of following cells\n");
+  for (let row = 0; row<universeSize;row++) {
+    for (let column = 0; column<universeSize;column++) {
+     initialUniverseState[row][column] = readline.question("[ "+row+" , "+column+" ]  :  ");
+    }
+  }
+  requiredGeneration = +readline.question("Please enter the reequired generation of given state \n");
 }
 
 handleUserInput();
