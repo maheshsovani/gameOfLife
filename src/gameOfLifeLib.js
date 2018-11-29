ce207ff22e839332729ada90c56e161f80cfc4de
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-11-29 14:09:23 +0530

    Removed readline and handleUserInput function.

--- src/gameOfLifeLib.js
+++ src/gameOfLifeLib.js
@@ -1,19 +1,4 @@
-const readline = require('readline-sync');
 const {zipper, validateNeighbours, contains} = require('./gameOfLifeUtil.js');
-
-const handleUserInput = function(){
-  let universeSize = +readline.question("Please enter size of the universe \n");
-  let initialUniverseState = new Array(universeSize).fill(0).map(x => []); 
-  let requiredGeneration;
-  console.log("Enter the states of following cells\n");
-  for (let row = 0; row<universeSize;row++) {
-    for (let column = 0; column<universeSize;column++) {
-      initialUniverseState[row][column] = readline.question("[ "+row+" , "+column+" ]  :  ");
-    }
-  }
-  requiredGeneration = +readline.question("Please enter the required generation of given state \n");
-  return initialUniverseState;
-}
 
 const findNeighboursOfCell = function(cell,endPoint) {
   let xCoordinates = [cell[0]-1, cell[0], cell[0]+1];
