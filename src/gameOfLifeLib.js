c36adf1193bc605329057c314aadab340ee96d87
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-11-29 12:47:34 +0530

    Renamed the function checkAlive to nextGeneration.
    Now nextGeneration accepts currentGeneration and bounds.

--- src/gameOfLifeLib.js
+++ src/gameOfLifeLib.js
@@ -8,70 +8,75 @@ const handleUserInput = function(){
   console.log("Enter the states of following cells\n");
   for (let row = 0; row<universeSize;row++) {
     for (let column = 0; column<universeSize;column++) {
-     initialUniverseState[row][column] = readline.question("[ "+row+" , "+column+" ]  :  ");
+      initialUniverseState[row][column] = readline.question("[ "+row+" , "+column+" ]  :  ");
     }
   }
   requiredGeneration = +readline.question("Please enter the required generation of given state \n");
   return initialUniverseState;
 }
 
-const findNeighboursOfCell = function(cell,universeSize) {
+const findNeighboursOfCell = function(cell,endPoint) {
   let xCoordinates = [cell[0]-1, cell[0], cell[0]+1];
   let yCoordinates = [cell[1]-1, cell[1], cell[1]+1];
   let zip = zipper(yCoordinates);
   let allNeighbours = xCoordinates.reduce(zip, []);
   allNeighbours.splice(4,1);
-  validateNeighbour = validateNeighbours.bind(null,universeSize-1);
+  validateNeighbour = validateNeighbours.bind(null,endPoint);
   let allValidNeighbours = allNeighbours.filter(validateNeighbour);
   return allValidNeighbours;
 }
 
-const findAllNeighbours = function(universeSize){
+const findAllNeighbours = function(bounds){
   let allNeighbours = {}
-  for (let row = 0; row<universeSize;row++) {
-    for (let column = 0; column<universeSize;column++) {
-      allNeighbours["["+row+", "+column+"]"] = findNeighboursOfCell([row,column], universeSize);
+  let startPoint= bounds.topLeft[0];
+  let endPoint= bounds.bottomRight[0];
+  for (let row = startPoint; row<=endPoint;row++) {
+    for (let column = startPoint; column<=endPoint ; column++) {
+      allNeighbours["["+row+", "+column+"]"] = findNeighboursOfCell([row, column], endPoint);
     }
   }
   return allNeighbours;
 }
 
-calculateAliveNeighboursOfCell = function(allNeighbours, currentGeneration, cell){
-    let numberOfAliveNeighbours = 0;
-    let neighboursOfCell = allNeighbours[cell];
-    for (let neighbour of neighboursOfCell){
-      let isAlive = contains(currentGeneration, neighbour); 
-      if(isAlive) { numberOfAliveNeighbours++; }
-    }
-    return numberOfAliveNeighbours;
+const calculateAliveNeighboursOfCell = function(allNeighbours, currentGeneration, cell){
+  let numberOfAliveNeighbours = 0;
+  let neighboursOfCell = allNeighbours[cell];
+  for (let neighbour of neighboursOfCell){
+    let isAlive = contains(currentGeneration, neighbour); 
+    if(isAlive) { numberOfAliveNeighbours++; }
+  }
+  return numberOfAliveNeighbours;
 }
 
-const aliveNeighboursCalculator = function(result, cell) {
-  result[cell] = calculateAliveNeighboursOfCell(cell);
-  return result;
+const aliveNeighboursCalculator = function(allNeighbours, currentGeneration) {
+  return function(result, cell) {
+    result[cell] = calculateAliveNeighboursOfCell(allNeighbours, currentGeneration, cell);
+    return result;
+  }
 }
 
 
 const calculateAliveNeighbours = function(allNeighbours, currentGeneration){
   let cells = Object.keys(allNeighbours);
-  calculateAliveNeighboursOfCell = calculateAliveNeighboursOfCell.bind(null, allNeighbours, currentGeneration);
-  let neighboursState = cells.reduce(aliveNeighboursCalculator, {});
+  let aliveNeighboursOfEachCell = aliveNeighboursCalculator(allNeighbours, currentGeneration);
+  let neighboursState = cells.reduce(aliveNeighboursOfEachCell, {});
   return neighboursState;
 }
 
-const checkAlive = function(allNeighbours,currentGeneration){
+const nextGeneration = function(currentGeneration,bounds){
+  let allNeighbours = findAllNeighbours(bounds);
   let neighboursState = calculateAliveNeighbours(allNeighbours,currentGeneration);
   let aliveCells = [];
   let allCells = Object.keys(neighboursState);
   for (let cell of allCells){
-    if(neighboursState[cell] == 2 || neighboursState[cell] == 3 ){
+    if(neighboursState[cell] == 3 ){
+      aliveCells.push(JSON.parse(cell));
+    }
+    if(neighboursState[cell] == 2 && contains(currentGeneration,JSON.parse(cell))){
       aliveCells.push(JSON.parse(cell));
     }
   }
   return aliveCells;
 }
 
-let currentGeneration = [[0,1],[1,1],[2,1]];
-let allNeighbours = findAllNeighbours(4);
-console.log(checkAlive(allNeighbours,currentGeneration));
-module.exports = {findNeighboursOfCell, findAllNeighbours, calculateAliveNeighbours};
+module.exports = {findNeighboursOfCell, findAllNeighbours, calculateAliveNeighbours, nextGeneration};
--- test/libTest.js
+++ test/libTest.js
@@ -5,13 +5,13 @@ const {findNeighboursOfCell, findAllNeighbours , calculateAliveNeighbours} = req
 describe("findNeighboursOfCell ",function(){
   it("should return an array of three neighbours",function(){
     let expectedOutput = [[0,1],[1,0],[1,1]];
-    assert.deepEqual(findNeighboursOfCell([0,0],3),expectedOutput);
+    assert.deepEqual(findNeighboursOfCell([0,0],2),expectedOutput);
     expectedOutput = [[0,1],[1,1],[1,2]];
-    assert.deepEqual(findNeighboursOfCell([0,2],3),expectedOutput);
+    assert.deepEqual(findNeighboursOfCell([0,2],2),expectedOutput);
     expectedOutput = [[1,0],[1,1],[2,1]];
-    assert.deepEqual(findNeighboursOfCell([2,0],3),expectedOutput);
+    assert.deepEqual(findNeighboursOfCell([2,0],2),expectedOutput);
     expectedOutput = [[1,1],[1,2],[2,1]];
-    assert.deepEqual(findNeighboursOfCell([2,2],3),expectedOutput);
+    assert.deepEqual(findNeighboursOfCell([2,2],2),expectedOutput);
   });
 
   it("should return an array of eight neighbours",function(){
@@ -29,26 +29,30 @@ describe("findAllNeighbours ",function(){
   const allOutputs = fs.readFileSync('./test/testData/findAllNeighboursOutput.json','utf8').split('\n\n');
   it("should return an object of neighbours of nine cells",function(){
     let expectedOutput = JSON.parse(allOutputs[0]);
-    assert.deepEqual(findAllNeighbours(3),expectedOutput);
+    let bounds = {topLeft: [0,0], bottomRight: [2, 2]};
+    assert.deepEqual(findAllNeighbours(bounds),expectedOutput);
   })
-  it("should return an object of neighbours of nine cells",function(){
+  it("should return an object of neighbours of sixteen cells",function(){
     let expectedOutput = JSON.parse(allOutputs[1]);
-    assert.deepEqual(findAllNeighbours(4),expectedOutput);
+    let bounds = {topLeft: [0,0], bottomRight: [3, 3]};
+    assert.deepEqual(findAllNeighbours(bounds),expectedOutput);
   })
-  it("should return an object of neighbours of nine cells",function(){
+  it("should return an object of neighbours of twenty five cells",function(){
     let expectedOutput = JSON.parse(allOutputs[2]);
-    assert.deepEqual(findAllNeighbours(5),expectedOutput);
+    let bounds = {topLeft: [0,0], bottomRight: [4, 4]};
+    assert.deepEqual(findAllNeighbours(bounds),expectedOutput);
   })
 });
 
 describe("calculateAliveNeighbours",function(){
   it("should return an object of having four keys of four cell and their respective alive neighbours",function(){
-    let initialGeneration = [['A', 'D'],['D', 'A']];
+    let initialGeneration = [[0, 0], [1, 1]];
     let allNeighbours = { '[0,0]' : [[0,1],[1,0],[1,1]],
                           '[0,1]' : [[0,0],[1,0],[1,1]],
                           '[1,0]' : [[0,0],[0,1],[1,1]],
                           '[1,1]' : [[0,0],[0,1],[1,0]] };
     let expectedOutput = {'[0,0]' : 1, '[0,1]' : 2, '[1,0]' : 2, '[1,1]' : 1}
-    assert.deepEqual(calculateAliveNeighbours(allNeighbours, initialGeneration),expectedOutput);
+    let actual = calculateAliveNeighbours(allNeighbours, initialGeneration);
+    assert.deepEqual(actual, expectedOutput);
   })
 });
